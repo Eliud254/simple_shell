e73f426d40140d889d9dba257feac76a796300c6
abel <abelamare3022@gmail.com>
2023-09-23 16:36:46 +0300

    f

---
+++ main.c
@@ -0,0 +1,50 @@
+#include "shell.h"
+
+/**
+ * main - The main function of the shell.
+ * Return: Always returns 0.
+ */
+int main(void)
+{
+	char *command = NULL;
+	size_t commandSize = 0;
+
+	while (1)
+	{
+		ssize_t bytesRead;
+
+		displayPrompt();
+		bytesRead = getline(&command, &commandSize, stdin);
+		if (bytesRead == -1)
+		{
+			if (feof(stdin))
+				break;
+
+			perror("Input error");
+			clearerr(stdin);
+			free(command);
+			continue;
+		}
+		if (command[bytesRead - 1] == '\n')
+			command[bytesRead - 1] = '\0';
+
+		if (command[0] != '#')
+			processCommand(command);
+	}
+	free(command);
+	return (0);
+}
+
+/**
+ * printEnvironment - Prints the current environment variables.
+ */
+void printEnvironment(void)
+{
+	int i = 0;
+
+	while (environ[i] != NULL)
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
+}
